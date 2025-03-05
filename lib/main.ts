4d2f7daab286f09ac0f415de98b1eb9d7a8c84a1
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-03-05 13:45:34 +0100

    fix: fix identity export

--- lib/main.ts
+++ lib/main.ts
@@ -2,7 +2,7 @@ export * as tasklist from './tasklist';
 export {} from './process-management';
 export * as operate from './operate';
 export {} from './optimize';
-export {} from './identity';
+export * as identity from './identity';
 export * as management from './management';
 export {
 	problemDetailsSchema,
