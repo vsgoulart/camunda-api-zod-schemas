778f3ac20383890aca2f78cbe0bd27487254ad6d
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-04-30 03:13:46 +0200

    feat: implement query variables endpoint

--- lib/process-management.ts
+++ lib/process-management.ts
@@ -1,5 +1,11 @@
 import { z } from 'zod';
-import { API_VERSION, type Endpoint } from './common';
+import {
+	advancedStringFilterSchema,
+	API_VERSION,
+	getCollectionResponseBodySchema,
+	getQueryRequestBodySchema,
+	type Endpoint,
+} from './common';
 
 const variableSchema = z.object({
 	name: z.string(),
@@ -22,7 +28,36 @@ const getVariable: Endpoint<Pick<Variable, 'variableKey'>> = {
 	},
 };
 
-const endpoints = { getVariable } as const;
+const queryVariablesRequestBodySchema = getQueryRequestBodySchema({
+	sortFields: ['name', 'value', 'fullValue', 'tenantId', 'variableKey', 'scopeKey', 'processInstanceKey'] as const,
+	filter: variableSchema
+		.pick({
+			tenantId: true,
+			isTruncated: true,
+		})
+		.merge(
+			z.object({
+				name: advancedStringFilterSchema.optional(),
+				value: advancedStringFilterSchema.optional(),
+				variableKey: advancedStringFilterSchema.optional(),
+				scopeKey: advancedStringFilterSchema.optional(),
+				processInstanceKey: advancedStringFilterSchema.optional(),
+			}),
+		),
+});
+type QueryVariablesRequestBody = z.infer<typeof queryVariablesRequestBodySchema>;
 
-export { endpoints, variableSchema };
-export type { Variable };
+const queryVariablesResponseBodySchema = getCollectionResponseBodySchema(variableSchema);
+type QueryVariablesResponseBody = z.infer<typeof queryVariablesResponseBodySchema>;
+
+const queryVariables: Endpoint = {
+	method: 'POST',
+	getUrl() {
+		return `/${API_VERSION}/variables/search`;
+	},
+};
+
+const endpoints = { getVariable, queryVariables } as const;
+
+export { endpoints, variableSchema, queryVariablesRequestBodySchema, queryVariablesResponseBodySchema };
+export type { Variable, QueryVariablesRequestBody, QueryVariablesResponseBody };
